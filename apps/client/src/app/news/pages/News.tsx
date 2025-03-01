1e90536fff03908a75bee7f030d3c78950306ba4
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-03-01 13:22:03 +0100

    mostrar texto cuando no hay articulos

--- apps/client/src/app/news/pages/News.tsx
+++ apps/client/src/app/news/pages/News.tsx
@@ -5,6 +5,7 @@ import NewCard from '../components/card/NewCard';
 import TabNavigation from '../components/ui/TabNavigation';
 import { ToastContainer } from 'react-toastify';
 import { Pagination } from '../components/pagination/Pagination';
+import { Archive } from 'lucide-react';
 
 export const News = () => {
   const [isAchieved, setIsAchieved] = useState(false);
@@ -24,18 +25,31 @@ export const News = () => {
         title2="Archived"
       />
 
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {useNewsQuery.data.news.map((newData) => {
-          return <NewCard key={newData._id} newData={newData} />;
-        })}
-      </section>
+      {useNewsQuery.data.news.length === 0 ? (
+        <div className="flex flex-col justify-center items-center py-12">
+          <div className="bg-indigo-100 p-3 rounded-full inline-flex mb-4">
+            <Archive size={24} className="text-indigo-600" />
+          </div>
+          <h3 className="text-xl font-medium text-gray-700 mb-2">
+            No hay artículos
+          </h3>
+        </div>
+      ) : (
+        <>
+          <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {useNewsQuery.data.news.map((newData) => {
+              return <NewCard key={newData._id} newData={newData} />;
+            })}
+          </section>
 
-      <Pagination
-        page={page}
-        totalPages={useNewsQuery.data.totalPages}
-        nextPage={nextPage}
-        prevPage={prevPage}
-      />
+          <Pagination
+            page={page}
+            totalPages={useNewsQuery.data.totalPages}
+            nextPage={nextPage}
+            prevPage={prevPage}
+          />
+        </>
+      )}
 
       <ToastContainer />
     </>
