5ae5cbe43fad714a02177b69b917255bdb16087b
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-03-01 13:44:51 +0100

    react hook form installation

---
+++ apps/client/src/app/news/pages/WriteNew.tsx
@@ -0,0 +1,5 @@
+export const WriteNew = () => {
+  return (
+    <div>WriteNew</div>
+  )
+}
--- apps/client/src/router/RouterConfig.tsx
+++ apps/client/src/router/RouterConfig.tsx
@@ -1,5 +1,6 @@
 import { NewById } from '../app/news/pages/NewById';
 import { News } from '../app/news/pages/News';
+import { WriteNew } from '../app/news/pages/WriteNew';
 
 export const routes: { path: string; element: React.ReactNode }[] = [
   // Aquí se añaden las rutas de la aplicación
@@ -10,5 +11,9 @@ export const routes: { path: string; element: React.ReactNode }[] = [
   {
     path: '/news/:id',
     element: <NewById />,
+  },
+  {
+    path: '/news/write',
+    element: <WriteNew />,
   },
 ];
